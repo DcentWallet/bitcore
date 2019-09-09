ba4a82ae9e18fb19ed2e1b97b13d896927b78f6d
sangyoonKim <sangyoon@iotrust.kr>
2019-09-09 19:07:11 +0900

    change ts config rule

--- packages/bitcore-node/src/routes/status.ts
+++ packages/bitcore-node/src/routes/status.ts
@@ -2,7 +2,6 @@ import express = require('express');
 import { PerformanceTracker } from '../decorators/Loggify';
 import config from '../config';
 import { ChainNetwork } from '../types/ChainNetwork';
-import { StateStorage } from '../models/state';
 const router = express.Router({ mergeParams: true });
 
 router.get('/enabled-chains', function(_, res) {
@@ -17,14 +16,6 @@ router.get('/enabled-chains', function(_, res) {
 
 router.get('/performance', function(_, res) {
   res.json(PerformanceTracker);
-});
-
-router.get('/:chain/:network/sync', async function(req, res) {
-  let { chain, network } = req.params;
-  const state = await StateStorage.collection.findOne({});
-  const initialSyncComplete =
-    state && state.initialSyncComplete && state.initialSyncComplete.includes(`${chain}:${network}`);
-  res.json({ initialSyncComplete });
 });
 
 module.exports = {
